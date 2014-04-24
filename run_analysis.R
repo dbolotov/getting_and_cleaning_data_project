34b0db16ea60a36750d57a3500c289c20c341659
dbolotov <dbolotov@live.com>
2014-04-23 23:03:00 -0700

    added code to read in files

--- run_analysis.R
+++ run_analysis.R
@@ -3,8 +3,17 @@
 # 1. Merges the training and the test sets to create one data set.
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 # 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive activity names.
+# 4. Appropriately labels the data set with descriptive variable or feature (column) names.
 # 5. Creates a second, independent tidy data set with the average of each 
 #    variable for each activity and each subject.
 
 
+
+setwd("E:/documents/progr/getting_and_cleaning_data_project")
+
+X_test <- as.data.frame(read.table("UCI HAR Dataset/test/X_test.txt"))
+Y_test <- as.data.frame(read.table("UCI HAR Dataset/test/Y_test.txt"))
+X_train <- as.data.frame(read.table("UCI HAR Dataset/train/X_train.txt"))
+Y_train <- as.data.frame(read.table("UCI HAR Dataset/train/Y_train.txt"))
+
+main <- rbind(cbind(X_train,Y_train),cbind(X_test,Y_test))
